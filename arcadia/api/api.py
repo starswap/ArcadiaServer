b4ef40b893962ca47db4e0a02cc9a31d05d840a2
Quentin Duff <30555853+qduff@users.noreply.github.com>
2022-07-30 12:59:40 +0100

    Improve API

--- arcadia/api/api.py
+++ arcadia/api/api.py
@@ -9,25 +9,32 @@ import math
 
 api_bp = Blueprint('api_bp', __name__)
 
-@api_bp.route("/guess", methods = ['GET', 'POST'])
-def recieve_guess():
+
+@api_bp.route("/game/find", methods = ['GET', 'POST'])
+def get_first_clue():
+    #needs to be sent current location of user in request
+    return ""
+
+
+@api_bp.route("/game/<arcade_id>/guess", methods = ['GET', 'POST'])
+def recieve_guess(arcade_id):
     if request.method == 'POST':
         user_x = request.values.get('xcoord')
         user_y = request.values.get('ycoord')
-        arcade_id = request.values.get('arcade_id')
+        # arcade_id = request.values.get('arcade_id')
         arcade_x, arcade_y = arcadeLocation.get_arcade_coords(arcade_id)
         distance = hs.haversine((arcade_x, arcade_y), (user_x, user_y), unit= Unit.METERS)
         direction = 'placeholder'
         response_dict = {'direction': direction, 'distance': distance}
-        response = api_bp.response_class(
-        response=json.dumps(response_dict),
-        status=200,
-        mimetype='application/json'
+        
+        return api_bp.response_class(
+            response=json.dumps(response_dict),
+            status=200,
+            mimetype='application/json'
         )
-    return response
 
-@api_bp.route("/getfirstclue", methods = ['GET', 'POST'])
-def get_first_clue():
+@api_bp.route("/game/<id>/clue", methods = ['GET', 'POST'])
+def get_clue():
     #needs to be sent current location of user in request
     return ""
 
