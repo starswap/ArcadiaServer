0a2d804a36cd94b4805400f3285f7866ff822243
TomSmail <thomas.c2.smail@gmail.com>
2022-07-30 22:34:08 +0100

    more fixed SQL

--- arcadia/api/badgeAssigner/badgeAssigner.py
+++ arcadia/api/badgeAssigner/badgeAssigner.py
@@ -5,14 +5,14 @@ def check_and_assign_badge(user_name, arcade_id, score):
     #if users score is highest in arcade give them a badge
     # user cant get badge if they already have it
     # user can only get badge if they are the highest scorer
-    cur.execute("SELECT UserID FROM 'Users' WHERE UserName=%s;", (user_name,))
+    cur.execute('SELECT UserID FROM "Users" WHERE UserName=%s;', (user_name,))
     user_id = cur.fetchone()
-    cur.execute("SELECT UserID, Score FROM 'Scores' WHERE ArcadeId =%s ORDER BY Score;", (arcade_id,))   
+    cur.execute('SELECT UserID, Score FROM "Scores" WHERE ArcadeId =%s ORDER BY Score;', (arcade_id,))   
     high_scorer, saved_score = cur.fetchone()
-    cur.execute("SELECT BadgeID FROM 'Badges' WHERE BadgeName=%s;", ("High Scorer",))
+    cur.execute('SELECT BadgeID FROM "Badges" WHERE BadgeName=%s;', ("High Scorer",))
     badge_id = cur.fetchone()
     if high_scorer != user_id and saved_score < score:
-        cur.execute(("INSERT INTO 'UserBadges' (UserID, BadgeID) VALUES (%s, %s);"),(user_id, badge_id))
+        cur.execute(('INSERT INTO "UserBadges" (UserID, BadgeID) VALUES (%s, %s);'),(user_id, badge_id))
         response = "You have earned a badge!"
         return response
     else:
