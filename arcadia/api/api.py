14db72f84b47aecfa8ff00f4712031fb6503b016
TomSmail <thomas.c2.smail@gmail.com>
2022-07-30 11:59:48 +0100

    better responses

--- arcadia/api/api.py
+++ arcadia/api/api.py
@@ -6,12 +6,10 @@ from haversine import Unit
 import math
 
 
-app = Flask(__name__)
-app.config["DEBUG"] = True
 
 api_bp = Blueprint('api_bp', __name__)
 
-@app.route("/guess", methods = ['GET', 'POST'])
+@api_bp.route("/guess", methods = ['GET', 'POST'])
 def recieve_guess():
     if request.method == 'POST':
         user_x = request.values.get('xcoord')
@@ -20,9 +18,15 @@ def recieve_guess():
         arcade_x, arcade_y = arcadeLocation.get_arcade_coords(arcade_id)
         distance = hs.haversine((arcade_x, arcade_y), (user_x, user_y), unit= Unit.METERS)
         direction = 'placeholder'
-    return direction, distance
+        response_dict = {'direction': direction, 'distance': distance}
+        response = api_bp.response_class(
+        response=json.dumps(response_dict),
+        status=200,
+        mimetype='application/json'
+        )
+    return response
 
-@app.route("/getfirstclue", methods = ['GET', 'POST'])
+@api_bp.route("/getfirstclue", methods = ['GET', 'POST'])
 def get_first_clue():
     #needs to be sent current location of user in request
     return ""
