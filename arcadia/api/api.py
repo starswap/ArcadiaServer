743164ef610c422b912d6225eec728cfc57f43c5
Quentin Duff <30555853+qduff@users.noreply.github.com>
2022-07-30 13:03:17 +0100

    Fix method

--- arcadia/api/api.py
+++ arcadia/api/api.py
@@ -16,23 +16,24 @@ def find_games():
     return "todo"
 
 
-@api_bp.route("/game/<arcade_id>/guess", methods = ['GET', 'POST'])
+@api_bp.route("/game/<arcade_id>/guess", methods = ['POST'])
 def recieve_guess(arcade_id):
-    if request.method == 'POST':
-        user_x = request.values.get('xcoord')
-        user_y = request.values.get('ycoord')
-        # arcade_id = request.values.get('arcade_id')
-        arcade_x, arcade_y = arcadeLocation.get_arcade_coords(arcade_id)
-        distance = hs.haversine((arcade_x, arcade_y), (user_x, user_y), unit= Unit.METERS)
-        direction = 'placeholder'
-        response_dict = {'direction': direction, 'distance': distance}
-        
-        return Response(
-            response=json.dumps(response_dict),
-            status=200,
-            mimetype='application/json'
-        )
+    user_x = request.values.get('xcoord')
+    user_y = request.values.get('ycoord')
+    # arcade_id = request.values.get('arcade_id')
+    arcade_x, arcade_y = arcadeLocation.get_arcade_coords(arcade_id)
+    distance = hs.haversine((arcade_x, arcade_y), (user_x, user_y), unit= Unit.METERS)
+    direction = 'placeholder'
+    response_dict = {'direction': direction, 'distance': distance}
+    
+    return Response(
+        response=json.dumps(response_dict),
+        status=200,
+        mimetype='application/json'
+    )
 
+
+#! I am not sure this is necessary?
 @api_bp.route("/game/<arcade_id>/clue", methods = ['GET', 'POST'])
 def get_clue(arcade_id):
     #needs to be sent current location of user in request
