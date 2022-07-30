e96cfc4d73361edfe94b16d02e6552245997f3a2
Quentin Duff <30555853+qduff@users.noreply.github.com>
2022-07-30 19:59:25 +0100

    Lots of fixes

--- arcadia/__init__.py
+++ arcadia/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 
 from .api import api
 from .home import home
-from .account import account
 from .arcadiaApp import arcadiaApp
 
 def init_app():
@@ -15,7 +14,6 @@ def init_app():
 
         app.register_blueprint(home.home_bp)
         app.register_blueprint(api.api_bp, url_prefix="/api")
-        app.register_blueprint(account.account_bp, url_prefix="/account")
         app.register_blueprint(arcadiaApp.app_bp, url_prefix="/app")
 
         return app
--- arcadia/arcadiaApp/arcadiaApp.py
+++ arcadia/arcadiaApp/arcadiaApp.py
@@ -1,11 +1,53 @@
-from flask import Blueprint, render_template
-from .requiremobile import requiremobile
+from flask import Blueprint, render_template, session
+from arcadia.arcadiaApp.requiremobile import requiremobile
+from flask import Blueprint, request
+from passlib.hash import argon2
+from requests import Response
+from arcadia.db import get_db
+from arcadia.arcadiaApp.requiremobile import requiremobile
+
 # from flask import current_app as app
 
-app_bp = Blueprint('app_bp', __name__, template_folder="templates", static_folder="static", static_url_path="/hst")
+app_bp = Blueprint('app_bp', __name__, template_folder="templates", static_folder="static", static_url_path="/ast")
 
 @app_bp.route('/')
 @requiremobile
 def home():
-    return render_template("displayguesses.jinja2")
+    if session.get("userid"):
+        return render_template("displayguesses.jinja2")
+    else:
+        return render_template("displayguesses.jinja2")
 
+
+
+@app_bp.route('/register')
+@requiremobile
+def account():
+    if request.method == 'POST':
+        username = request.values.get('username')
+        password = request.values.get('password')
+        hashed_password = argon2.hash(password)
+
+        db, cur = get_db()
+        cur.execute("""INSERT INTO 'Users' (UserName, PasswordHash) VALUES (%s,%s);""",
+        (username, hashed_password))
+
+    return Response(
+        status = 200,
+        mimetype='application/json'
+    )
+
+@app_bp.route('/login')
+@requiremobile
+def login():
+    if request.method == 'POST':
+        username = request.values.get('username')
+        password = request.values.get('password')
+        hashed_password = argon2.hash(password)
+    
+        
+        
+    return Response(
+        status = 200,
+        mimetype='application/json'
+    )
