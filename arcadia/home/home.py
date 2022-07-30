0beb58e99cad44da15ecb934b38cfec6bca14df1
Quentin Duff <30555853+qduff@users.noreply.github.com>
2022-07-30 12:58:09 +0100

    Do main page

--- arcadia/home/home.py
+++ arcadia/home/home.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 # from flask import current_app as app
 
-home_bp = Blueprint('home_bp', __name__, template_folder="templates", static_folder="static")
+home_bp = Blueprint('home_bp', __name__, template_folder="templates", static_folder="static", static_url_path="/hst")
 
 @home_bp.route('/')
 def home():
