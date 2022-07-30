06ff4c0c516e74dd2fdfee14fdd9cda78cee6386
Quentin Duff <30555853+qduff@users.noreply.github.com>
2022-07-30 22:18:02 +0100

    Fixes

--- arcadia/arcadiaApp/arcadiaApp.py
+++ arcadia/arcadiaApp/arcadiaApp.py
@@ -4,18 +4,19 @@ from flask import Blueprint, request
 from passlib.hash import argon2
 from requests import Response
 from arcadia.db import get_db
-from arcadia.arcadiaApp.requiremobile import requiremobile
 
 # from flask import current_app as app
-app_bp = Blueprint('app_bp', __name__, template_folder="templates", static_folder="static", static_url_path="/ast")
+app_bp = Blueprint('app_bp', __name__, template_folder="templates",
+                   static_folder="static", static_url_path="/ast")
+
 
 @app_bp.route('/')
 @requiremobile
 def home():
-    if session.get("userid"):
+    if session.get("UserID"):
         return render_template("apphome.jinja2")
     else:
-        return render_template("apphome.jinja2")
+        return redirect(url_for("app_bp.login"))
 
 
 @app_bp.route('/guesser')
@@ -29,47 +30,70 @@ def guesser():
 def register():
 
     if request.method == 'GET':
+        if session.get("UserID"):
+            redirect(url_for("app_bp.apphome"))
         return render_template("register.jinja2")
 
     elif request.method == 'POST':
-        username = request.values.get('username')
-        password = request.values.get('password')
-        hashed_password = argon2.hash(password)
+        if not session.get("UserID"):
+            username = request.values.get('username')
+            password = request.values.get('password')
+            hashed_password = argon2.hash(password)
 
-        db, cur = get_db()
-        cur.execute("""INSERT INTO 'Users' (UserName, PasswordHash) VALUES (%s,%s);""",
-        (username, hashed_password))
-        
-        cur.execute("""SELECT UserID FROM 'Users' WHERE UserName=%s;""", (username,))
-        session['UserID'] = cur.fetchone()
-        session['UserName'] = username
-        return redirect(url_for("app_bp.home"))
+            db, cur = get_db()
+            cur.execute("""INSERT INTO 'Users' (UserName, PasswordHash) VALUES (%s,%s);""",
+                        (username, hashed_password))
+
+            cur.execute(
+                """SELECT UserID FROM 'Users' WHERE UserName=%s;""", (username,))
+            session['UserID'] = cur.fetchone()
+            session['UserName'] = username
+            return redirect(url_for("app_bp.home"))
+        else:
+            return "already logged in, logout before registering"
 
 
-@app_bp.route('/login', methods=["GET","POST"])
+@app_bp.route('/login', methods=["GET", "POST"])
 @requiremobile
 def login():
 
     if request.method == 'GET':
+        if session.get("UserID"):
+            redirect(url_for("app_bp.apphome"))
         return render_template("login.jinja2",)
-
     elif request.method == 'POST':
-        username = request.values.get('username')
-        password = request.values.get('password')
-        hashed_password = argon2.hash(password)
-        
-        db, cur = get_db()
-        cur.execute("""SELECT PasswordHash FROM 'Users' WHERE UserName= %s;""", (username,))
-        resp = cur.fetchone()
+        if not session.get("UserID"):
+            username = request.values.get('username')
+            password = request.values.get('password')
+            hashed_password = argon2.hash(password)
 
-        if resp == None:
-            return redirect(url_for("account_bp.login"))
+            db, cur = get_db()
+            cur.execute(
+                """SELECT PasswordHash FROM 'Users' WHERE UserName= %s;""", (username,))
+            resp = cur.fetchone()
 
-        if not argon2.verify(password, resp["PasswordHash"]):
-            return redirect(url_for("account_bp.login"))
+            if resp == None:
+                return redirect(url_for("account_bp.login"))
 
+            if not argon2.verify(password, resp["PasswordHash"]):
+                return redirect(url_for("account_bp.login"))
+
+            else:
+                cur.execute(
+                    """SELECT UserID FROM 'Users' WHERE UserName=%s;""", (username,))
+                session['UserID'] = cur.fetchone()
+                session['UserName'] = username
+                return redirect(url_for("app_bp.home"))
         else:
-            cur.execute("""SELECT UserID FROM 'Users' WHERE UserName=%s;""", (username,))
-            session['UserID'] = cur.fetchone()
-            session['UserName'] = username
-            return redirect(url_for("app_bp.home"))
+            return "already logged in, logout before logging in again"
+
+
+@app_bp.route('/logout')
+@requiremobile
+def logout():
+    if session.get("UserID"):
+        session.pop("UserID")
+    if session.get("UserName"):
+        session.pop("UserName")
+    return redirect(url_for("app_bp.home"))
+
