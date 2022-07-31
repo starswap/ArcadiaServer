baee135488438b29df78ed617dd8ee9fd6b7de6c
Hamish Starling <hamishstarling@hotmail.co.uk>
2022-07-31 10:21:14 +0100

    Add Target distance constants

--- arcadia/api/api.py
+++ arcadia/api/api.py
@@ -5,6 +5,7 @@ from arcadia.api.modlocation import getGameType, returnNearCoords, getArcadeCoor
 from arcadia.api.modlocation.distdir import bearingToCardinal, dist, direc
 
 
+TARGET_DISTANCE = 10
 
 api_bp = Blueprint('api_bp', __name__)
 
@@ -48,7 +49,7 @@ def receive_guess(arcade_id):
     direction = direc(userlat, userlong, arcade_x, arcade_y)
     bearing = bearingToCardinal(direction)
 
-    if distance < 5:
+    if distance < TARGET_DISTANCE:
         return Response(
             response=json.dumps({"success": "gamepermitted", "gamecode": getGameType(arcade_id)}),
             status=200,
